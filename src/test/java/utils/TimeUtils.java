491917beab463b79c16dd78893c0eddce3cf65a4
Kristiyan Stoychev <77746043+kristiyanbstoychev@users.noreply.github.com>
2023-06-23 14:20:00 +0300

    Update TimeUtils.java

--- src/test/java/utils/TimeUtils.java
+++ src/test/java/utils/TimeUtils.java
@@ -1,16 +1,11 @@
 package utils;
-
-import org.junit.jupiter.api.Assertions;
-import org.openqa.selenium.ElementNotInteractableException;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-
 import java.util.List;
 
-import static tests.BaseTest.getWait;
-import static tests.BaseTest.scrollIntoView;
+import static tests.ExampleBaseTest.scrollIntoView;
 
 public class TimeUtils {
+
     public static void waitForSeconds(int timeoutInSeconds) {
         try {
             Thread.sleep(timeoutInSeconds * 1000L);
@@ -19,38 +14,33 @@ public class TimeUtils {
         }
     }
 
-    public static void waitForElementToBeDisplayedList(int waitingTimeInSeconds, List<WebElement> webElement, int webElementIndex, String elementDescription) {
-        for (int i = 1; i < waitingTimeInSeconds; i++) {
-            try {
-                Assertions.assertTrue(webElement.get(webElementIndex).isDisplayed());
-                System.out.println(elementDescription);
+    public static void clickAvailableElementFromList(List<WebElement> webElementList) {
+        int i = 0;
+        while (!webElementList.get(i).isDisplayed()) {
+            System.out.println("************* element not clickable...retrying *************");
+            i++;
+            if (i > 5) {
+                System.out.println("************* element cannot be found *************");
                 break;
-            } catch (Exception e) {
-                //                System.out.println("Couldn't find element, retrying. . . #" + i);
-            }
-            TimeUtils.waitForSeconds(1);
-        }
-    }
-
-    //Finds the clickable element on the page, if more than one is present in the DOM
-    public static void waitForClickableElement(List<WebElement> webElements, int numberOfElementsToBeChecked) {
-        int elementNumber = 0;
-
-        for (int i = 0; i < numberOfElementsToBeChecked; i++) {
-            try {
-                getWait(1).until(ExpectedConditions.elementToBeClickable(webElements.get(i)));
-                elementNumber = i;
-                break;
-            } catch (Exception e) {
-                System.out.println("Waiting for the element to be clickable");
             }
         }
-        try {
-            scrollIntoView(webElements.get(elementNumber));
-            System.out.println("************* Element found *************");
-            webElements.get(elementNumber).click();
-        } catch (Exception e) {
-            throw new ElementNotInteractableException("Element not displayed");
-        }
+        scrollIntoView(webElementList.get(i));
+
+        webElementList.get(i).click();
     }
+
+    public static void clickAvailableElement(WebElement webElement) {
+        int i = 0;
+        while (!webElement.isDisplayed()) {
+            System.out.println("element not clickable");
+            i++;
+            if (i >= 5) {
+                System.out.println("element cannot be found");
+                break;
+            }
+        }
+        scrollIntoView(webElement);
+        webElement.click();
+    }
+
 }
