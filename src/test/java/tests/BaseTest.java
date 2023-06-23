75902d8d6f1e0831a1b0120b1c7697a4d8581db8
Kristiyan Stoychev <77746043+kristiyanbstoychev@users.noreply.github.com>
2023-06-23 14:19:12 +0300

    Update BaseTest.java

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,16 +1,11 @@
 package tests;
 
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
-import io.cucumber.java.Scenario;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -19,42 +14,55 @@ import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.safari.SafariOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import utils.TestDataGeneration;
 import utils.TimeUtils;
 
-import java.io.File;
-import java.io.IOException;
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
 
+import utils.TestResultsWatcher;
+import utils.CSVHandler;
+
 import static tests.GlobalVariables.browserForTestingEnvVariableName;
 import static tests.GlobalVariables.cfuiURL;
+import static tests.GlobalVariables.csvEmailsList;
+import static tests.GlobalVariables.csvNicknamesList;
+import static tests.GlobalVariables.defaultBrowser;
+import static tests.GlobalVariables.defaultDevice;
 import static tests.GlobalVariables.deviceForTestingEnvVariableName;
 import static tests.GlobalVariables.driver;
-import static tests.GlobalVariables.getBrowserForTestingEnvVariable;
-import static tests.GlobalVariables.getDeviceForTestingEnvVariable;
-import static tests.GlobalVariables.localTempDirectory;
-import static tests.GlobalVariables.operationSystem;
+import static tests.GlobalVariables.getEnvVariable;
+import static tests.GlobalVariables.isDebugModeEnabled;
+import static tests.GlobalVariables.isMobile;
+import static tests.GlobalVariables.urlForTestingEnvVariable;
 import static tests.GlobalVariables.webDriverManager;
 
 //Execute tests in alphabetical order
 @TestMethodOrder(MethodOrderer.MethodName.class)
-public class BaseTest {
+
+//Enables the TestWatcher class, which monitors the test results (currently used only for taking screenshots if a test fails)
+@ExtendWith(TestResultsWatcher.class)
+public class ExampleBaseTest {
 
     //Executed before each test
-    @Before
+    @BeforeEach
     public void doBeforeEachTest() {
-        pickBrowser(getBrowserForTestingEnvVariable(browserForTestingEnvVariableName));
+        pickBrowser(getEnvVariable(browserForTestingEnvVariableName, defaultBrowser));
 
-        if (getBrowserForTestingEnvVariable(browserForTestingEnvVariableName).equals("mobile")) {
-            GlobalVariables.isMobile = true;
+        if (getEnvVariable(browserForTestingEnvVariableName, defaultBrowser).equals("mobile")) {
+            isMobile = true;
         }
 
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
-        driver.navigate().to(cfuiURL);
-//        getWait().until(ExpectedConditions.presenceOfElementLocated((By.xpath("xpath of an element that should be displayed on the home page"))));
+        driver.navigate().to(getEnvVariable(urlForTestingEnvVariable, cfuiURL));
+
+        //Sets the content of email and onsite notifications
+        GlobalVariables.setEmailAndNotificationsContent();
+
+        //Reads a couple of csv files, storing emails and nicknames of users, needed for testing
+        CSVHandler.readCSVFileAndSaveContentAsListOfStrings("testData/emails.csv", csvEmailsList);
+        CSVHandler.readCSVFileAndSaveContentAsListOfStrings("testData/nicknames.csv", csvNicknamesList);
     }
 
     //Picks which browser and browser options should be used for testing
@@ -62,11 +70,12 @@ public class BaseTest {
         switch (browser) {
             case "chrome": {
                 webDriverManager = WebDriverManager.chromedriver();
+                webDriverManager.setup();
                 ChromeOptions chromeOptions = new ChromeOptions();
                 chromeOptions.addArguments("--incognito");
                 chromeOptions.addArguments("--ignore-certificate-errors");
                 //if tests are executed in debug mode, start the browser in a non-headless window
-                if (!GlobalVariables.isDebugModeEnabled) {
+                if (!isDebugModeEnabled) {
                     chromeOptions.addArguments("--headless", "--window-size=1920,1200");
                 }
                 chromeOptions.addArguments("--allow-running-insecure-content");
@@ -81,9 +90,10 @@ public class BaseTest {
             }
             case "firefox": {
                 webDriverManager = WebDriverManager.firefoxdriver();
+                webDriverManager.setup();
                 FirefoxOptions firefoxOptions = new FirefoxOptions();
                 firefoxOptions.addArguments("--incognito");
-                if (!GlobalVariables.isDebugModeEnabled) {
+                if (!isDebugModeEnabled) {
                     firefoxOptions.addArguments("--headless", "--window-size=1920,1200");
                 }
                 firefoxOptions.addArguments("--lang=en_US");
@@ -93,18 +103,20 @@ public class BaseTest {
             }
             case "safari": {
                 webDriverManager = WebDriverManager.safaridriver();
+                webDriverManager.setup();
                 SafariOptions safariOptions = new SafariOptions();
                 break;
             }
             case "mobile": {
                 webDriverManager = WebDriverManager.chromedriver();
+                webDriverManager.setup();
                 Map<String, String> mobileEmulation = new HashMap<>();
-                mobileEmulation.put("deviceName", getDeviceForTestingEnvVariable(deviceForTestingEnvVariableName));
+                mobileEmulation.put("deviceName", getEnvVariable(deviceForTestingEnvVariableName, defaultDevice));
                 ChromeOptions mobileOptions = new ChromeOptions();
                 mobileOptions.setExperimentalOption("mobileEmulation", mobileEmulation);
                 mobileOptions.addArguments("--incognito");
                 mobileOptions.addArguments("--ignore-certificate-errors");
-                if (!GlobalVariables.isDebugModeEnabled) {
+                if (!isDebugModeEnabled) {
                     mobileOptions.addArguments("--headless");
                 }
                 mobileOptions.addArguments("--allow-running-insecure-content");
@@ -120,19 +132,6 @@ public class BaseTest {
         }
     }
 
-    @After
-    public void afterMethod(Scenario scenario) {
-        if (scenario.isFailed()) {
-            if (operationSystem.contains("Windows")) {
-                scenario.getName();
-                String screenshotFileName = localTempDirectory + scenario.getName() + "_" + TestDataGeneration.formatDate("hh-mm-ss_dd.MM", "bg") + ".jpeg";
-                System.out.println("Test failed! Screenshot saved to: " + screenshotFileName);
-                takeSnapShot(driver, screenshotFileName);
-            }
-        }
-        driver.quit();
-    }
-
     //Scrolls the page to a desired element
     public static void scrollIntoView(WebElement element) {
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView({block: 'center', inline: 'nearest'});", element);
@@ -146,24 +145,4 @@ public class BaseTest {
     public static WebDriverWait getWait() {
         return getWait(15);
     }
-
-    //method used for taking screenshots
-    public static void takeSnapShot(WebDriver webdriver, String fileWithPath) {
-        //Convert web driver object to TakeScreenshot
-        TakesScreenshot scrShot = ((TakesScreenshot) webdriver);
-
-        //Call getScreenshotAs method to create image file
-        File SrcFile = scrShot.getScreenshotAs(OutputType.FILE);
-
-        //Move image file to new destination
-        File DestFile = new File(fileWithPath);
-
-        //Copy file at destination
-        try {
-            FileUtils.copyFile(SrcFile, DestFile);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
 }
